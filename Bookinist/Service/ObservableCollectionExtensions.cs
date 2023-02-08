4b43943c006b4136317174898f08ee35cc69905e
nibred <oleksii.derbin@gmail.com>
2023-02-08 15:23:13 +0100

    added oc extensions

---
+++ Bookinist/Service/ObservableCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections.ObjectModel;
+
+public static class ObservableCollectionExtensions
+{
+    public static void AddWithClear<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        collection.Clear();
+        collection.Add(items);
+    }
+    public static void Add<T>(this ObservableCollection<T> collection, IEnumerable<T> items)
+    {
+        foreach (T item in items)
+        {
+            collection.Add(item);
+        }
+    }
+}
--- Bookinist/ViewModels/StatisticViewModel.cs
+++ Bookinist/ViewModels/StatisticViewModel.cs
@@ -15,9 +15,9 @@ namespace Bookinist.ViewModels;
 
 internal class StatisticViewModel : ViewModelBase
 {
-    private IRepository<Buyer> _buyerRepository;
-    private IRepository<Book> _booksRepository;
-    private IRepository<Deal> _dealsRepository;
+    private readonly IRepository<Buyer> _buyerRepository;
+    private readonly IRepository<Book> _booksRepository;
+    private readonly IRepository<Deal> _dealsRepository;
 
     public ObservableCollection<Bestseller> Bestsellers { get; } = new();
 
@@ -25,15 +25,23 @@ internal class StatisticViewModel : ViewModelBase
     public ICommand BestsellersCommand => _bestsellersCommand ??= new RelayCommandAsync(BestsellersCommandExecuteAsync);
     private async Task BestsellersCommandExecuteAsync()
     {
-        var bestsellers = await _dealsRepository.Items.GroupBy(d => d.Book)
-            .Select(deals => new { Book = deals.Key, Count = deals.Count() })
-            .OrderByDescending(book => book.Count)
+        var bestsellers = await _dealsRepository.Items //TODO check 'sum' function
+            .GroupBy(d => d.Book)
+            .Select(deals => new
+            {
+                Book = deals.Key,
+                Count = deals.Count()
+            })
             .Take(5)
-            .Select(k => new Bestseller { Name = k.Book.Name, Count= k.Count })
-            .ToListAsync();
+            .OrderByDescending(d => d.Count)
+            .Select(i => new Bestseller
+            {
+                Name = i.Book.Name,
+                Count = i.Count
+            })
+            .ToArrayAsync();
 
-        Bestsellers.Clear();
-        bestsellers.ForEach(i => Bestsellers.Add(i));
+        Bestsellers.AddWithClear(bestsellers);
     }
 
     public StatisticViewModel(IRepository<Buyer> buyerRepository, IRepository<Book> booksRepository, IRepository<Deal> dealsRepository)
